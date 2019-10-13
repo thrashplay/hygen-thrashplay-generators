9ad0cf51b41d0d03878931431ab524c7672360ef
Sean Kleinjung <sean@thrashplay.com>
2019-10-13 01:36:23 -0500

    fix(create-thrashplay-lib): correct path to missing package.json

--- packages/bootstrap-library/_templates/.lib/thrashplay-common.js
+++ packages/bootstrap-library/_templates/.lib/thrashplay-common.js
@@ -1,29 +1,31 @@
 const _ = require('lodash')
 const path = require('path')
 const appRoot = require('app-root-path')
-const packageJson = require(path.resolve(process.cwd(), 'package.json'))
 
-const fromPackageJson = (path) => {
-  return _.get(packageJson, path)
-}
-
-const getPackageName = () => {
-  const nameParts = fromPackageJson('name').split('/')
+const getPackageName = (packageJson) => {
+  const nameParts = _.get(packageJson, 'name').split('/')
   return nameParts.length > 1 ? nameParts[1] : nameParts[0]
 }
 
-const getPackageVersion = () => {
-  return '^' + fromPackageJson('version')
+const parsePackageJson = () => {
+  try {
+    const packageJsonPath = path.resolve(process.cwd(), 'package.json')
+    if (fs.existsSync(packageJsonPath)) {
+      const packageJson = require(packageJsonPath)
+      return {
+        packageLicense: _.get(packageJson, 'license'),
+        packageName: getPackageName(packageJson),
+      }
+    }
+  } catch (err) {}
+  return {}
 }
 
 module.exports = {
   withDefaultArguments: (args) => _.merge({}, args, {
-    createThrashplayLibVersion: getPackageVersion(),
+    ...parsePackageJson(),
     createThrashplayLibScriptsDir: path.resolve(appRoot.path, 'node_modules', '@thrashplay/bootstrap-library', 'dist'),
     projectDir: path.resolve(process.cwd()),
-    packageLicense: fromPackageJson('license'),
-    packageName: getPackageName(),
     ...args,
-    templateSourceDir: path.resolve(appRoot.path, 'node_modules', '@thrashplay/bootstrap-library', 'dist', 'templates'),
   })
 }

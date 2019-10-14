7e56e30105868d8fedab12d88d39df8b96bf4d99
Sean Kleinjung <sean@thrashplay.com>
2019-10-14 14:32:31 -0500

    chore: Add logic to skip git push if no files changed by Lerna.

--- .ci/push-tags.sh
+++ .ci/push-tags.sh
@@ -3,10 +3,14 @@
 set -e
 set -x
 
-if [ -z "$DRONE_COMMIT_SHA" ]
-then
-      echo "Refusing to force push, because no original commit SHA was provided."
-      exit 1
+if [ -z "$(git status --porcelain)" ]; then
+  echo "No changed files detected, skipping git push."
+  exit 0
+fi
+
+if [ -z "$DRONE_COMMIT_SHA" ]; then
+  echo "Refusing to force push, because no original commit SHA was provided."
+  exit 1
 fi
 
 # for debugging sake, just log what's changed
